1fbe85cfcb5b32375c1978741742326e8982528a
Julio Sampaio <iamjuliosampaio@gmail.com>
2018-12-25 18:38:25 -0200

    Initial commit

---
+++ GraphQL-SimpleTalk/Startup.cs
@@ -0,0 +1,46 @@
+﻿using GraphiQl;
+using GraphQL_SimpleTalk.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GraphQL_SimpleTalk
+{
+    public class Startup
+    {
+        public const string GraphQlPath = "/graphql";
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddScoped<BlogService>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.UseGraphiQl(GraphQlPath);
+        }
+    }
+}
